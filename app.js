d8c0e037a662572495ebf7bf043b3f1d67fedc32
Zack Moyal <zack.moyal@gmail.com>
2024-04-20 04:19:05 -0700

    completed Exercises 1-5

--- app.js
+++ app.js
@@ -4,7 +4,44 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Zack";
+let lastName;
+var age;
 // Exercise 2
+firstName;
+lastName = "Moyal";
+age = 29;
+
+console.log(`${firstName} ${lastName} is ${29} years old.`);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`It is ${isCaseSensitive} that ${language} was created in ${createdYear}.`);
+
+// Exercise 4
+let product = "Vans shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `The ${product} is priced at ${price}. It's ${isOnSale} that it is on sale
+and the stock is ${stock}, so it is ${inStock} that it is in stock. But if
+I were to buy one, it would be size ${selectedSize}. I heard that when it
+is in stock again, they will be running a sale having all shirts ${salePercentage}% off.`
+);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} long. Your last bookmark was page ${bookmark}.`);
